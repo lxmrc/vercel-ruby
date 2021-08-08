07f69abaeae4f14b2e96a59fe9ac37ccb7ef6c41
Alex <alex@lxmrc.com>
2021-08-08 13:41:35 +1000

    Update index.rb

--- api/index.rb
+++ api/index.rb
@@ -1,5 +1,5 @@
 Handler = Proc.new do |req, res|
   res.status = 200
-  res['Content-Type'] = 'text/text; charset=utf-8'
+  res['Content-Type'] = 'text/html; charset=utf-8'
   res.body = "Current Time: #{Time.new}"
 end
