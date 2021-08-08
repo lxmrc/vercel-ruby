5c98f23778914772b924838cc79f8c5df1c0f6ef
Alex <alex@lxmrc.com>
2021-08-08 13:40:28 +1000

    Use Vercel docs example

--- api/index.rb
+++ api/index.rb
@@ -1,10 +1,5 @@
 Handler = Proc.new do |req, res|
-	res.status = 200
-	res['Content-Type'] = 'text/text; charset=utf-8'
-	if req.query.has_key?("name")
-		name = req.query["name"]
-		res.body = "Hello, #{name}!"
-	else
-		res.body = "Hello, stranger!"
-	end
+  res.status = 200
+  res['Content-Type'] = 'text/text; charset=utf-8'
+  res.body = "Current Time: #{Time.new}"
 end
